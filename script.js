468d871dec3753f5f319e9fbb4c24989b1bb17d4
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2023-11-27 17:43:26 -0600

    made some updates to functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var timerElement= document.getElementById("timer-block");
 var startButton= document.getElementById("start-button");
+var quizSection= document.getElementById("quiz-section");
 
 var isCorrect= false;
 var numberCorrect= 0;
@@ -11,6 +12,7 @@ function startQuiz() {
     timerCount = 75;
     startButton.disabled = true;
     startTimer();
+    beginQuizQuestions();
   }
   
 function startTimer() {
@@ -23,6 +25,10 @@ function startTimer() {
         clearInterval(timer);
     }
     }, 1000);
+    
+  }
+
+  function beginQuizQuestions(){
     
   }
 
